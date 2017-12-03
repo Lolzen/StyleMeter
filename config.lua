9fbd98ba1eb304859999184761f3c3c55bc47ef3
Georg Eitler <funer@gmx.at>
2017-12-03 15:14:20 +0100

    config: initialize

---
+++ config.lua
@@ -0,0 +1,8 @@
+--//config//--
+
+local addon, ns = ...
+
+ns.cfg = {
+	["current_mode"] = true,					-- Automatically switches to current fight data whenever a new fight starts, and back to overall data when it ends
+	["real_value"] = false,						-- Substracts overdamage; only real damage dealt is shown
+}
